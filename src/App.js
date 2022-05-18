3df3cdd693611f15e17c3feb0333c494e0a3a3c1
safym <osafym@gmail.com>
2022-05-18 23:28:51 +0300

    first (actual) commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="header">
+        <a className="button" href="">Main</a>
+        <a className="button" href="">Profile</a> 
+        <a className="button" href="">News</a>
+        <a className="button" href="">Login</a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
