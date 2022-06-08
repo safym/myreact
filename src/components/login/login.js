9844a588b6038239adddf30acd39df97169509d1
safym <osafym@gmail.com>
2022-06-09 01:09:04 +0300

    добавила инпуты из  библиотеки mui

--- src/components/login/login.js
+++ src/components/login/login.js
@@ -1,20 +1,38 @@
 // import components
+import TextField from '@mui/material/TextField';
+import { alpha, styled } from '@mui/material/styles';
+
+styled(TextField)({})
 
 const HomePage = () => {
   return (
     <div className="login">
         <div className="username">
-            <label>Name:</label>
-            <input type="text" name="name" /> 
+            <CssTextField label="Login" id="Login" margin="dense"/>
         </div>
 
         <div className="password">
-            <label>Password:</label>
-            <input type="password" name="name" /> 
+            <CssTextField label="Password" id="Password" margin="dense"/>
         </div>
 
     </div>
   );
 };
+
+const CssTextField = styled(TextField)({
+    '& label.Mui-focused': {
+      color: '#ae7cd8',
+    },
+    
+    '& .MuiOutlinedInput-root': {
+      '& fieldset': {
+        borderColor: '#ae7cd8',
+      },
+      
+      '&.Mui-focused fieldset': {
+        borderColor: '#ae7cd8',
+      },
+    },
+  });
 
 export default HomePage;
