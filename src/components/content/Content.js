104adf54ecf0564d0b9489bf2a9d5aaeb8f2d67d
safym <osafym@gmail.com>
2022-06-19 17:10:01 +0300

    сделала добавление новостей из профиля #5

--- src/components/content/Content.js
+++ src/components/content/Content.js
@@ -1,34 +1,27 @@
-import News1 from'./ContentNews';
+import News1 from "./ContentNews";
 
-const Content= () => {
-    return(
-      <div className="Content">
-        <News />
-
-        <News1 />
-      </div>);
-  }
+const Content = () => {
+  return (
+    <div className="Content">
+      <News />
+      <News1 />
+    </div>
+  );
+};
 
 const News = () => {
-  
-  var currentNews = JSON.parse(localStorage.getItem('news'));
+  var currentNews = JSON.parse(localStorage.getItem("news"));
+  const final = [];
 
-  // currentNews.forEach(newsObjects => {
-  //   for (let key in newsObjects) {
-  //       console.log(`${key}: ${newsObjects[key]}`);
-  //   }
-  // });
-
-  for (var i = 0; i < currentNews.length; i++ ) {
-    // console.log(currentNews[i]);
+  for (var i = currentNews.length-1; i >= 0; i--)  {
+    final.push(
+      <div className="NewsContent">
+        <h1>{currentNews[i].title}</h1>
+        <p>{currentNews[i].text}</p>
+      </div>
+    );
   }
+  return <div className="news">{final}</div>;
+};
 
-  return(
-    <div className="NewsContent">
-      <h1>test title</h1>
-      <p>test text</p>
-    </div>);
-}
-
-
-  export default Content;
+export default Content;
