ff2c6102f1a0e6c25b6ee53c5f69e44840d1ae00
safym <osafym@gmail.com>
2022-06-19 20:45:22 +0300

    кое что подправила #8

--- src/components/home/article.js
+++ src/components/home/article.js
@@ -2,9 +2,9 @@ const Article= () => {
     return(
             <div className="Article">
                 <h1>Foxter</h1>
-                <p>Consectetur velit sint culpa reprehenderit amet magna ut minim commodo sunt.</p> 
-                <p>Ipsum laborum officia culpa qui aliqua laborum elit aute laboris excepteur ullamco qui aute.</p>
-                <p>Ex aute cupidatat incididunt mollit dolore voluptate ad veniam sint fugiat adipisicing quis.</p>
+                <p>1. Sign in </p> 
+                <p>2. Make news</p>
+                <p>3. Wait for the doorbell to ring 👀</p>
             </div>);
   }
 
